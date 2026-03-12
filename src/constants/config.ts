cb6b41ea0e8dbeb3810843bf746991f484d539de
Steven <steven.quihuire0411@gmail.com>
2026-03-12 17:25:18 -0500

    Upload the project from my Portfolio

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,53 +40,49 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: 'Steven Quihuire — Developer',
+    fullName: 'Steven Quihuire',
+    email: 'steven.quihuire0411@mail.com',
   },
   hero: {
-    name: "John Doe",
-    p: ["I develop 3D visuals, user", "interfaces and web applications"],
+    name: 'Steven Quihuire',
+    p: ['Full Stack Developer: mobile, web, and desktop applications'],
   },
   contact: {
-    p: "Get in touch",
-    h2: "Contact.",
+    p: 'Get in touch',
+    h2: 'Contact.',
     form: {
       name: {
-        span: "Your Name",
+        span: 'Your Name',
         placeholder: "What's your name?",
       },
-      email: { span: "Your Email", placeholder: "What's your email?" },
+      email: { span: 'Your Email', placeholder: "What's your email?" },
       message: {
-        span: "Your Message",
-        placeholder: "What do you want to say?",
+        span: 'Your Message',
+        placeholder: 'What do you want to say?',
       },
     },
   },
   sections: {
     about: {
-      p: "Introduction",
-      h2: "Overview.",
-      content: `I'm a skilled software developer with experience in TypeScript and
-      JavaScript, and expertise in frameworks like React, Node.js, and
-      Three.js. I'm a quick learner and collaborate closely with clients to
-      create efficient, scalable, and user-friendly solutions that solve
-      real-world problems. Let's work together to bring your ideas to life!`,
+      p: 'Introduction',
+      h2: 'Overview.',
+      content: `Fullstack JavaScript Web Developer with over 2 years of experience in application support, specializing in JavaScript, React, Angular, .NET, Java, and Node.js. My experience includes maintaining, improving, and optimizing production web platforms, creating reusable and atomic components, developing microfrontends, troubleshooting, and supporting development and deployment processe!`,
     },
     experience: {
-      p: "What I have done so far",
-      h2: "Work Experience.",
+      p: 'What I have done so far',
+      h2: 'Work Experience.',
     },
     feedbacks: {
-      p: "What others say",
-      h2: "Testimonials.",
+      p: 'What others say',
+      h2: 'Testimonials.',
     },
     works: {
-      p: "My work",
-      h2: "Projects.",
+      p: 'My work',
+      h2: 'Projects.',
       content: `Following projects showcases my skills and experience through
     real-world examples of my work. Each project is briefly described with
-    links to code repositories and live demos in it. It reflects my
+    links privates to code repositories and live demos in it. It reflects my
     ability to solve complex problems, work with different technologies,
     and manage projects effectively.`,
     },
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,179 +1,171 @@
 import type {
+  TExperience,
   TNavLink,
+  TProject,
   TService,
   TTechnology,
-  TExperience,
   TTestimonial,
-  TProject,
-} from "../types";
+} from '../types';
 
 import {
-  mobile,
   backend,
   creator,
-  web,
-  javascript,
-  typescript,
-  html,
   css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mobile,
+  mongodb,
+  nodejs,
   reactjs,
   redux,
   tailwind,
-  nodejs,
-  mongodb,
-  git,
-  figma,
-  docker,
-  meta,
-  starbucks,
-  tesla,
-  shopify,
-  carrent,
-  jobit,
-  tripguide,
   threejs,
-} from "../assets";
+  typescript,
+  web,
+} from '../assets';
 
 export const navLinks: TNavLink[] = [
   {
-    id: "about",
-    title: "About",
+    id: 'about',
+    title: 'About',
   },
   {
-    id: "work",
-    title: "Work",
+    id: 'work',
+    title: 'Work',
   },
   {
-    id: "contact",
-    title: "Contact",
+    id: 'contact',
+    title: 'Contact',
   },
 ];
 
 const services: TService[] = [
   {
-    title: "Web Developer",
+    title: 'Web Developer',
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: 'React Native Developer',
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: 'Backend Developer',
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: 'Good practices',
     icon: creator,
   },
 ];
 
 const technologies: TTechnology[] = [
   {
-    name: "HTML 5",
+    name: 'HTML 5',
     icon: html,
   },
   {
-    name: "CSS 3",
+    name: 'CSS 3',
     icon: css,
   },
   {
-    name: "JavaScript",
+    name: 'JavaScript',
     icon: javascript,
   },
   {
-    name: "TypeScript",
+    name: 'TypeScript',
     icon: typescript,
   },
   {
-    name: "React JS",
+    name: 'React JS',
     icon: reactjs,
   },
   {
-    name: "Redux Toolkit",
+    name: 'Redux Toolkit',
     icon: redux,
   },
   {
-    name: "Tailwind CSS",
+    name: 'Tailwind CSS',
     icon: tailwind,
   },
   {
-    name: "Node JS",
+    name: 'Node JS',
     icon: nodejs,
   },
   {
-    name: "MongoDB",
+    name: 'MongoDB',
     icon: mongodb,
   },
   {
-    name: "Three JS",
+    name: 'Three JS',
     icon: threejs,
   },
   {
-    name: "git",
+    name: 'git',
     icon: git,
   },
   {
-    name: "figma",
+    name: 'figma',
     icon: figma,
   },
   {
-    name: "docker",
+    name: 'docker',
     icon: docker,
   },
 ];
 
 const experiences: TExperience[] = [
   {
-    title: "React.js Developer",
-    companyName: "Starbucks",
-    icon: starbucks,
-    iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    title: 'React.js Developer',
+    companyName: 'SABATA SIEMPRE SABATA',
+    icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBF-P3qklhbSYN011d67AskDSIOBLLeujTbQ&s',
+    iconBg: '#383E56',
+    date: 'November 2024 - September 2025',
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      'Development of a comprehensive website that centralizes the components and tools for different company products.',
+      'I improved product loading on the ecommerce site by implementing best practices in image and background image optimization.',
+      'Integration of registration and login methods using external providers.',
+      'I introduced the use of CD as a work strategy on GitHub, and I used Figma to accelerate design proposals.',
     ],
   },
   {
-    title: "React Native Developer",
-    companyName: "Tesla",
-    icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    title: 'React Native Developer',
+    companyName: 'SEGUROS EQUINOCCIAL',
+    icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYwI7Tl-Bv0mKfWrIL_WqXwzGJMv-_fensbg&s',
+    iconBg: '#E6DEDD',
+    date: 'July 2024 - November 2024',
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      'I resolved critical errors in the Equinoccial Insurance web service, improving system stability through the use of SQL Server.',
+      'I implemented best practices in SQL, achieving a significant optimization of the web systems performance.',
+      'I identified and fixed bugs, improving the performance of both the website and the database for a smoother user experience.',
+      'Participating in code reviews and providing constructive feedback to other developers.',
     ],
   },
   {
-    title: "Web Developer",
-    companyName: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
+    title: 'Web Developer FullStack',
+    companyName: 'ACADEMIA ELITE',
+    icon: 'https://aulavirtual-smartplus.com/src/icons/logo1.png',
+    iconBg: '#383E56',
+    date: 'January 2023 - May 2023',
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      'I designed and developed an educational web platform, integrating third-party services for a robust and scalable backend.',
+      'I used React, Next.js, GitHub, and Figma to create an attractive and functional user interface, improving the user experience and ensuring high performance standards.',
+      'I work with backend and frontend technologies such as React and .Net through contracts and signatures.',
     ],
   },
   {
-    title: "Full stack Developer",
-    companyName: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
+    title: 'Full stack Developer',
+    companyName: 'ARCHETICS',
+    icon: 'https://avatars.githubusercontent.com/u/82846905?s=200&v=4',
+    iconBg: '#E6DEDD',
+    date: 'last',
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      'I designed and developed a module in a production application that allows users to connect to the app even with location restrictions, improving the accessibility of the service.',
+      'I proposed and achieved acceptance of the interface design for the new module, creating a fluid and friendly user experience that facilitates navigation.',
+      'Hexagonal architecture alongside Java and Angular in the stack.',
+      'Learning about Microfrontends in system development',
     ],
   },
 ];
@@ -181,94 +173,95 @@ const experiences: TExperience[] = [
 const testimonials: TTestimonial[] = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      'Working with Steven was an excellent experience. During his time as a React.js Developer, he significantly improved the performance of our ecommerce site.',
+    name: 'Ing David Alarcon',
+    designation: 'RRHH',
+    company: 'GAD Calderon',
+    image: 'https://randomuser.me/api/portraits/men/4.jpg',
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+      'He proved to be a highly competent and dedicated developer. He quickly integrated into the team and successfully resolved critical errors in our web services.',
+    name: 'Mercedes Pillajo',
+    designation: 'Sales',
+    company: 'Entrepreneur',
+    image: 'https://randomuser.me/api/portraits/women/5.jpg',
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+      'She possesses a highly valuable combination of frontend and backend skills. During the development of our educational platform, she successfully built a scalable solution.',
+    name: 'Manuel Narvaez',
+    designation: 'Economist',
+    company: 'Fybeca',
+    image: 'https://randomuser.me/api/portraits/men/6.jpg',
   },
 ];
 
 const projects: TProject[] = [
   {
-    name: "Car Rent",
+    name: 'Medical Appointment Management System',
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      'Web platform for comprehensive medical appointment management that allows patients to book consultations online and doctors to view their schedule in real time.',
     tags: [
       {
-        name: "react",
-        color: "blue-text-gradient",
+        name: 'react',
+        color: 'blue-text-gradient',
       },
       {
-        name: "mongodb",
-        color: "green-text-gradient",
+        name: 'mongodb',
+        color: 'green-text-gradient',
       },
       {
-        name: "tailwind",
-        color: "pink-text-gradient",
+        name: 'Java',
+        color: 'pink-text-gradient',
       },
     ],
-    image: carrent,
-    sourceCodeLink: "https://github.com/",
+    image: 'https://expert.holanuna.com/es/practika/wp-content/uploads/2020/12/05_Docplanner.png',
+    sourceCodeLink: 'https://github.com/',
   },
   {
-    name: "Job IT",
+    name: 'Personal Finance App with Reports',
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      'A web application designed for personal finance management that allows you to record income and expenses organized by category, making it easier to control your cash flow. The system includes savings goals with automatic alerts based on events that trigger when certain limits or conditions are reached.',
     tags: [
       {
-        name: "react",
-        color: "blue-text-gradient",
+        name: 'react',
+        color: 'blue-text-gradient',
       },
       {
-        name: "restapi",
-        color: "green-text-gradient",
+        name: 'restapi',
+        color: 'green-text-gradient',
       },
       {
-        name: "scss",
-        color: "pink-text-gradient",
+        name: 'net',
+        color: 'pink-text-gradient',
       },
     ],
-    image: jobit,
-    sourceCodeLink: "https://github.com/",
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEfLzPXkNlYekF40Ry_3ela6FJqqguBrETyA&s',
+    sourceCodeLink: 'https://github.com/',
   },
   {
-    name: "Trip Guide",
+    name: 'E-commerce platform with real-time inventory',
     description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      'E-commerce application developed with a modern architecture that allows you to manage a product catalog, shopping cart, and order processing. It uses PostgreSQL for data persistence and Redis for session management and caching.',
     tags: [
       {
-        name: "nextjs",
-        color: "blue-text-gradient",
+        name: 'angular',
+        color: 'blue-text-gradient',
       },
       {
-        name: "supabase",
-        color: "green-text-gradient",
+        name: 'postgress',
+        color: 'green-text-gradient',
       },
       {
-        name: "css",
-        color: "pink-text-gradient",
+        name: 'node',
+        color: 'pink-text-gradient',
       },
     ],
-    image: tripguide,
-    sourceCodeLink: "https://github.com/",
+    image: 'https://ebz-static.s3.ap-south-1.amazonaws.com/easebuzz-static/upi-credit-cards-v1.png',
+    sourceCodeLink: 'https://github.com/',
   },
 ];
 
-export { services, technologies, experiences, testimonials, projects };
+export { experiences, projects, services, technologies, testimonials };
